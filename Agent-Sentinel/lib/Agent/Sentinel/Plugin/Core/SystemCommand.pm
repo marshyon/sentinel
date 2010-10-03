d87f5767ee49bd4558d8e5ab604e0389c9d3743d
marshyon <marshyon@gmail.com>
2010-10-03 11:07:29 +0100

    modified to do usefull things - using IPC::Open3 - capturing STDOUT and STDERR

--- Agent-Sentinel/lib/Agent/Sentinel/Plugin/Core/SystemCommand.pm
+++ Agent-Sentinel/lib/Agent/Sentinel/Plugin/Core/SystemCommand.pm
@@ -1,16 +1,72 @@
 package Agent::Sentinel::Plugin::Core::SystemCommand;
 
-use Moose; # automatically turns on strict and warnings
+use Moose;
 use Data::Dumper;
+use IPC::Open3;
+use YAML;
 
-has 'cfg' => (is => 'rw' );
-has 'task' => (is => 'rw' );
+use Symbol 'gensym';
+our $VERSION = '0.1';
+
+has 'cfg'           => ( is => 'rw' );
+has 'task'          => ( is => 'rw' );
+has 'stdout'        => ( is => 'ro' );
+has 'stderr'        => ( is => 'ro' );
+has 'pid'           => ( is => 'ro' );
 
 sub run {
-      my $self = shift;
-      # be busy for a bit
-      sleep 1;
-      return "SystemCommand task[" . $self->task . "] [" . $self->{'cfg'}->{'command'} . "]";
+
+    my $self = shift;
+    my ( $wtr, $rdr, $err );
+    my %dump = ();
+   
+    # construct command line argument from config
+    my $args = '';
+    if( $self->cfg->{'args'} =~ m{ARRAY} ) {
+        $args = join(" ", @{ $self->cfg->{'args'} } );
+    }
+    else {
+        $args = $self->cfg->{'args'};
+    }
+    my $command .= $self->cfg->{'command'} . ' ' . $args;
+
+    $err = gensym;
+    my $pid = open3( $wtr, $rdr, $err, $command );
+    $self->{'pid'} = $pid;
+    $dump{'pid'} = $pid;
+    $dump{'cfg'} = $self->cfg;
+
+    $dump{'debug'} = '[' . $self->cfg->{'command'} . ' ' . $args . ']';
+
+    my $yaml_status_file  = $self->cfg->{'logfile'};
+    YAML::DumpFile( $yaml_status_file, %dump ) if $yaml_status_file;
+    close $wtr;
+
+    while(<$rdr>) {
+        $dump{'rdr'} .= $_;
+    }
+    close $rdr;
+
+    while(<$err>) {
+        $dump{'err'} .= $_;
+    }
+    close $err;
+
+    YAML::DumpFile( $yaml_status_file, %dump ) if $yaml_status_file;
+
+    if( $dump{'err'} ) {
+    return
+        "ERROR - SystemCommand task["
+      . $self->task . "] log [". $yaml_status_file ."] ["
+      . $command . "]" . $dump{'err'};
+    }
+    else {
+        return
+            "SystemCommand task["
+          . $self->task . "] log [". $yaml_status_file ."] ["
+          . $command . "]";
+    }
+
 }
 
 1;
